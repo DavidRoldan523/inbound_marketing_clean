35df6f6c980075d5a14f90dac55d4448be124f96
cjgalvisc96 <cjgalvisc@unal.edu.co>
2019-07-02 18:22:37 -0500

    Respond CSV in Service

--- Back-End/API/API/settings.py
+++ Back-End/API/API/settings.py
@@ -40,6 +40,18 @@ INSTALLED_APPS = [
     'inboud_transform',
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_csv.renderers.CSVRenderer',
+    ),
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- Back-End/API/inboud_transform/views/service_view.py
+++ Back-End/API/inboud_transform/views/service_view.py
@@ -2,33 +2,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 import pandas as pd
-
-
-def transform_csv(file, email_column_name):
-    pass
-
-
-def transform_excel(file, email_column_name):
-    excel = pd.ExcelFile(file.open('r'))
-    print(excel.sheet_names)
-    #print(file)
-
-
+import csv
+from django.http import HttpResponse
 
 @api_view(['POST'])
 def service(request):
     try:
-        email_column_name = request.data.get('email_column_name')
-        file = request.FILES.get['file']
-        if (file.name.split('.')[1]).lower() == 'csv':
-            response = delimiter = request.data.get('delimiter')
-            transform_csv(file, email_column_name)
-        else:
-            response = transform_excel(file, email_column_name)
-
-
-
-        return Response({'h': 'h'}, status.HTTP_200_OK)
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="export.csv"'
+        writer = csv.DictWriter(response, fieldnames=['emp_name', 'dept', 'birth_month'])
+        writer.writeheader()
+        writer.writerow({'emp_name': 'John Smith', 'dept': 'Accounting', 'birth_month': 'November'})
+        writer.writerow({'emp_name': 'Erica Meyers', 'dept': 'IT', 'birth_month': 'March'})
+        return response
     except Exception as e:
         return Response({'Error': e}, status.HTTP_400_BAD_REQUEST)
 
