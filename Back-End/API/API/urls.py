4973cc424d16e265b588115958354bb3b278b55d
David Roldán <jdavidroldan523@gmail.com>
2019-07-09 09:44:59 -0500

    fix production kik token

--- Back-End/API/API/urls.py
+++ Back-End/API/API/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from rest_framework.authtoken import views
 
 urlpatterns = [
     #Apps
     url(r'^api/v1/transform/', include('inboud_transform.urls')),
-    
-    # Token
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
+
     url(r'^admin/', admin.site.urls)
 ]
