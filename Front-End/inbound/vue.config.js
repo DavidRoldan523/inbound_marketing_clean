bb35a8bfa88aa2dad6cb8218cb4ddb93bc53741f
root <root@ip-172-31-41-79.ec2.internal>
2019-07-23 13:56:44 +0000

    fix error invalid host header

--- Front-End/inbound/vue.config.js
+++ Front-End/inbound/vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
     //...
     devServer: {
       open: process.platform === 'darwn',
+      disableHostCheck: true,
       host: '0.0.0.0',
       port: 8004, //CHANGE YOUR PORT HERE!
       https: false,
